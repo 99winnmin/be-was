ae0817cabeffcb18608fa04eca181b0c27dab0b4
99winnmin <ojysep9987@gmail.com>
2024-01-30 12:11:48 +0900

    :sparkles: Feat : QuestionSimpleInfo 리스트 반환 converter 추가

--- src/main/java/common/utils/JsonConverter.java
+++ src/main/java/common/utils/JsonConverter.java
@@ -1,5 +1,6 @@
 package common.utils;
 
+import domain.qna.query.dto.QuestionSimpleInfo;
 import domain.user.query.application.UserInfoResponse;
 import java.util.List;
 import java.util.Map;
@@ -25,6 +26,32 @@ public class JsonConverter {
             jsonBuilder.append("\n    \"userId\": \"").append(userInfoResponse.getUserId()).append("\",");
             jsonBuilder.append("\n    \"userName\": \"").append(userInfoResponse.getUserName()).append("\",");
             jsonBuilder.append("\n    \"email\": \"").append(userInfoResponse.getEmail()).append("\"");
+
+            jsonBuilder.append("\n  }");
+            i++;
+        }
+
+        jsonBuilder.append("\n]");
+
+        return jsonBuilder.toString();
+    }
+
+    public static String mapToListObjectJson2(List<QuestionSimpleInfo> questionSimpleInfoList) {
+        StringBuilder jsonBuilder = new StringBuilder();
+
+        jsonBuilder.append("[\n");
+
+        // 맵의 항목을 반복하여 JSON 문자열을 구성합니다.
+        int i = 0;
+        for (QuestionSimpleInfo questionSimpleInfo : questionSimpleInfoList) {
+            if (i == 0) jsonBuilder.append("  {" + "\n");
+            else if (i > 0) jsonBuilder.append(",\n  {");
+
+            jsonBuilder.append("\n    \"questionSimpleInfoId\": \"").append(questionSimpleInfo.getQuestionSimpleInfoId()).append("\",");
+            jsonBuilder.append("\n    \"questionId\": \"").append(questionSimpleInfo.getQuestionId()).append("\",");
+            jsonBuilder.append("\n    \"title\": \"").append(questionSimpleInfo.getTitle()).append("\",");
+            jsonBuilder.append("\n    \"createdAt\": \"").append(questionSimpleInfo.getCreatedAt()).append("\",");
+            jsonBuilder.append("\n    \"writerId\": \"").append(questionSimpleInfo.getWriterId()).append("\"");
 
             jsonBuilder.append("\n  }");
             i++;
